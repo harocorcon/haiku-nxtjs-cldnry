75f2e05507bca0a1be3ab12e8d9bf367677abbd5
harocorcon <hracconsular@gmail.com>
2025-02-02 21:21:10 +0800

    login fix

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -11,7 +11,7 @@ export default function Page(){
         <form action={formAction} className="max-w-xs mx-auto">
             <div className="mb-3">
                 <input name="username" autoComplete="off" type="text" placeholder="Username" className="input input-bordered w-full max-w-xs" />
-                {formState.errors?.username && (
+                {formState.success == false && (
                     <div role="alert" className="alert alert-error">
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
@@ -24,28 +24,13 @@ export default function Page(){
                         strokeWidth="2"
                         d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
                     </svg>
-                    <span>{formState.errors?.username}</span>
+                    <span>{formState.errors?.message}</span>
                   </div>
                 )}
             </div>
             <div className="mb-3">
                 <input name="password" autoComplete="off" type="password" placeholder="Password" className="input input-bordered w-full max-w-xs" />
-                {formState.errors?.password && (
-                    <div role="alert" className="alert alert-error">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="h-6 w-6 shrink-0 stroke-current"
-                      fill="none"
-                      viewBox="0 0 24 24">
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
-                    </svg>
-                    <span>{formState.errors?.password}</span>
-                  </div>
-                )}
+                
             </div>
             <button className="btn btn-primary">Submit</button>
         </form>
